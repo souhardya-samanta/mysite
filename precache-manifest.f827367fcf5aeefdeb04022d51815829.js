f7a955a9c7d13e3807e6b1c54b60efe990014c84
souhardya69 <samantasouhardya@hotmail.com>
2020-10-09 20:14:02 +0530

    Updates

---
+++ precache-manifest.f827367fcf5aeefdeb04022d51815829.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1572bd500ffa6b2401ed",
+    "url": "/mysite/static/css/main.cfd44417.chunk.css"
+  },
+  {
+    "revision": "1572bd500ffa6b2401ed",
+    "url": "/mysite/static/js/main.1572bd50.chunk.js"
+  },
+  {
+    "revision": "63fb5b41de279ed09cdd",
+    "url": "/mysite/static/js/1.63fb5b41.chunk.js"
+  },
+  {
+    "revision": "3ded49a2979a7fef5d5b",
+    "url": "/mysite/static/js/runtime~main.3ded49a2.js"
+  },
+  {
+    "revision": "42a3dbf698f6887f0ed10ce31d608fe3",
+    "url": "/mysite/index.html"
+  }
+];
